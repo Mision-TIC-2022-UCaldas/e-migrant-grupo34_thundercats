51c462fafce55b1165047192742aa3f7d3280eda
JulyMorales22 <jamorales859@misena.edu.co>
2021-11-26 11:37:01 -0500

    Creacion de capas

---
+++ Migrantes.App/Migrantes.App.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Migrantes.App.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ Migrantes.App/Migrantes.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Migrantes.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
