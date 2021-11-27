e0c89cfdf247d2b14a1da3d78ccca4b6167a46fa
giraldiego <giraldiego@gmail.com>
2021-11-27 13:28:15 -0500

    Agrega modelo para Entidad en capa Dominio

---
+++ Migrantes.App/Dominio/Entidad.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Migrantes.App.Dominio
+{
+
+    public enum Sector
+    {
+        Publico,
+        Privado,
+        SinAnimoDeLucro,
+        NoGubernamental,
+        Otro
+    }
+    public enum TipoServicio
+    {
+        Salud,
+        Juridicos,
+        Viveres,
+        ComidaPreparada,
+        Empleo,
+        AlojamientoTemporal,
+        AlojamientoPermanente,
+        Educacion,
+        Otros
+    }
+
+    public class Entidad
+    {
+        public string RazonSocial {get;set;}
+        public int Nit {get;set;}
+        public string Direccion {get;set;}
+        public string Ciudad {get;set;}
+        public string Telefono {get;set;}
+        public string DireccionElectronica {get;set;}
+        public string PaginaWeb {get;set;}
+
+        public Sector Sector {get; set;}
+        public TipoServicio TipoServicio {get; set;}
+
+    }
+}
