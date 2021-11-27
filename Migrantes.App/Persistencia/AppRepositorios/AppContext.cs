368c52b13c24294fdc82121c512934b5202a0460
giraldiego <giraldiego@gmail.com>
2021-11-27 14:12:30 -0500

    Agrega AppContext to capa de Persistencia

---
+++ Migrantes.App/Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+using Migrantes.App.Dominio;
+
+namespace Migrantes.App.Persistencia{
+
+    public class AppContext:DbContext{
+
+        public DbSet <Entidad> Entidad {get;set;}
+        // public DbSet <Migrante> Migrantes {get;set;}
+
+        // protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder ){
+        //     if (!optionsBuilder.IsConfigured){
+        //         optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB; Initial Catalog=Thundercats");
+        //     }
+        // }
+
+    }
+}
