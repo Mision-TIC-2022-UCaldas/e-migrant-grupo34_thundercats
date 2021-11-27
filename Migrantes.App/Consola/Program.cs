8981c7131e6b5fe70259bce59c93eb6df91062dd
giraldiego <giraldiego@gmail.com>
2021-11-27 14:08:50 -0500

    Agrega aplicacion de consola

---
+++ Migrantes.App/Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Migrantes.App.Dominio;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
