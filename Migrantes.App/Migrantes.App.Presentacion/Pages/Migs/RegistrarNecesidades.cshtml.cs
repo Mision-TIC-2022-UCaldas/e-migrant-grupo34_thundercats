f32fad101bb14060c2f98480770acf35f4961d62
giraldiego <giraldiego@gmail.com>
2021-11-28 09:16:45 -0500

    Agrega Registrar necesidad en capa Persistencia

---
+++ Migrantes.App/Migrantes.App.Presentacion/Pages/Migs/RegistrarNecesidades.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Migrantes.App.Dominio;
+using Migrantes.App.Persistencia;
+
+
+namespace Migrantes.App.Presentacion.Pages.Migs
+{
+    public class RegistrarNecesidadesModel : PageModel
+    {
+
+
+        private readonly IRepositorioMigrante _repoMigrante;
+        public Migrante Migrante {get;set;}
+
+        public string NumeroDocumento {get;set;}
+        public Necesidades Necesidades {get;set;}
+
+        public RegistrarNecesidadesModel(IRepositorioMigrante _repoMigrante){
+            this._repoMigrante = _repoMigrante;
+        }
+        public void OnGet(int NumeroDocumento)
+        {
+            Migrante= _repoMigrante.GetMigrante(NumeroDocumento);
+            Console.WriteLine(NumeroDocumento);
+
+            if(Migrante==null){
+                Console.WriteLine("No Encontrado");
+
+            }else{
+                Console.WriteLine("Encontrado");
+            }
+
+        }
+
+        public void OnPost(Migrante Migrante, Necesidades Necesidades){
+
+
+          //  Migrante = _repoMigrante.GetMigrante(NumeroDocumento);
+            _repoMigrante.AddNecesidades(Migrante.Id,Necesidades);
+            Console.WriteLine(Migrante.Id);
+
+            if(Necesidades==null){
+                Console.WriteLine("No hay nada en necesidades");
+            }
+            else{
+                Console.WriteLine("Oka");
+
+                Console.WriteLine(Necesidades.Descripcion);
+            }
+        }
+
+    }
+}
